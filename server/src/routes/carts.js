8701ef672eb7e08ff8dcb78fe6a9086028d1cf2f
Ladan Mirzaei <ladanmirzaei@MacBook-Air-von-Ladan.local>
2025-02-05 20:55:42 +0100

    Änderung server

---
+++ server/src/routes/carts.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getCart,
+  addToCart,
+  editCart,
+  cartCount,
+  deleteCart,
+} from "../controllers/cart.js";
+
+const router = Router();
+
+router.get("/", getCart); // Fetches the current user's cart
+router.post("/add", addToCart); // Adds an item to the user's cart
+router.post("/edit", editCart);
+router.get("/count", cartCount);
+router.delete("/delete", deleteCart);
+
+export default router;
